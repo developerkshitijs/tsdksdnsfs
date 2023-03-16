0d0f5059c17cfd3e7fc42a0353b3676f4cbaf422
developerssh <119683709+developerssh@users.noreply.github.com>
2023-03-16 19:24:54 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -30,7 +30,7 @@ async def progress_bar(current,total,reply,start):
             tot=hrb(total)
             cur=hrb(current)
             try:
-                await reply.edit(f'`┌ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 📈 -【 {perc} 】\n├ 𝙎𝙥𝙚𝙚𝙙 🧲 -【 {sp} 】\n└ 𝙎𝙞𝙯𝙚 📂 -【 {cur} / {tot} 】`')
+                await reply.edit(f'`┌ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 📥 -【 {perc} 】\n├ 𝙎𝙥𝙚𝙚𝙙 🚦 -【 {sp} 】\n└ 𝙎𝙞𝙯𝙚 🗂 -【 {cur} / {tot} 】`' Bot By Kshitij👨🏻‍💻)
                
             except FloodWait as e:
                 time.sleep(e.x)
