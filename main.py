5475df90b36ab78bc647e77cf2bf8ac146d0707d
developerssh <119683709+developerssh@users.noreply.github.com>
2023-03-16 20:14:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -299,7 +299,7 @@ async def account_login(bot: Client, m: Message):
 
 
             try:
-                Show = f"**Downloading:-📥**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`\n\n"
+                Show = f"**Downloading:-📥**\n\n**Name💬 :-** `{name}\nQuality📹 - {raw_text2}`\n\n**Url :-** `{url}`\n\n"
                 prog = await m.reply_text(Show)
                 cc = f'**Title »** {name1} {res}.mkv\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}'
                 cc1 =f'**Title »** {name1} {res}.pdf\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}'
@@ -309,7 +309,7 @@ async def account_login(bot: Client, m: Message):
                         await prog.delete (True)
                         time.sleep(1)
                         # await helper.send_doc(bot,m,cc,ka,cc1,prog,count,name)
-                        reply = await m.reply_text(f"Uploading - `{name}`")
+                        reply = await m.reply_text(f"Uploading🚥 - `{name}`")
                         time.sleep(1)
                         start_time = time.time()
                         await m.reply_document(ka,caption=cc1)
